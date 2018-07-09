97b252dc3d80cd41f3a8a41a5f101ce6d95c844b
AssurexCircuitry <40968199+AssurexCircuitry@users.noreply.github.com>
2018-07-09 08:23:35 +0100

    Updated unpack.bat
    To Unpack all files to One folder :3

--- unpack.bat
+++ unpack.bat
@@ -1,32 +1,32 @@
 @echo off
-EvolutionUnpack B.AnimRetarget.cache
-EvolutionUnpack B.CharacterCodesCache.cache
-EvolutionUnpack B.CharacterCodesCacheDx10.cache
-EvolutionUnpack B.CharacterCodesCacheDx11.cache
-EvolutionUnpack B.CharacterCodesCacheDx9.cache
-EvolutionUnpack B.Font.cache
-EvolutionUnpack B.Misc.cache
-EvolutionUnpack B.ShaderDx10.cache
-EvolutionUnpack B.ShaderDx11.cache
-EvolutionUnpack B.ShaderDx9.cache
-EvolutionUnpack B.ShaderPermutationDx10.cache
-EvolutionUnpack B.ShaderPermutationDx11.cache
-EvolutionUnpack B.ShaderPermutationDx9.cache
-EvolutionUnpack B.TextureDx9.cache
-EvolutionUnpack F.Misc.cache
-EvolutionUnpack F.TextureDx9.cache
-EvolutionUnpack H.AnimRetarget.cache
-EvolutionUnpack H.CharacterCodesCache.cache
-EvolutionUnpack H.CharacterCodesCacheDx10.cache
-EvolutionUnpack H.CharacterCodesCacheDx11.cache
-EvolutionUnpack H.CharacterCodesCacheDx9.cache
-EvolutionUnpack H.Font.cache
-EvolutionUnpack H.Misc.cache
-EvolutionUnpack H.ShaderDx10.cache
-EvolutionUnpack H.ShaderDx11.cache
-EvolutionUnpack H.ShaderDx9.cache
-EvolutionUnpack H.ShaderPermutationDx10.cache
-EvolutionUnpack H.ShaderPermutationDx11.cache
-EvolutionUnpack H.ShaderPermutationDx9.cache
+EvolutionUnpack B.AnimRetarget.cache /game
+EvolutionUnpack B.CharacterCodesCache.cache /game
+EvolutionUnpack B.CharacterCodesCacheDx10.cache /game
+EvolutionUnpack B.CharacterCodesCacheDx11.cache /game
+EvolutionUnpack B.CharacterCodesCacheDx9.cache /game
+EvolutionUnpack B.Font.cache /game
+EvolutionUnpack B.Misc.cache /game
+EvolutionUnpack B.ShaderDx10.cache /game
+EvolutionUnpack B.ShaderDx11.cache /game
+EvolutionUnpack B.ShaderDx9.cache /game
+EvolutionUnpack B.ShaderPermutationDx10.cache /game
+EvolutionUnpack B.ShaderPermutationDx11.cache /game
+EvolutionUnpack B.ShaderPermutationDx9.cache /game
+EvolutionUnpack B.TextureDx9.cache /game
+EvolutionUnpack F.Misc.cache /game
+EvolutionUnpack F.TextureDx9.cache /game
+EvolutionUnpack H.AnimRetarget.cache /game
+EvolutionUnpack H.CharacterCodesCache.cache /game
+EvolutionUnpack H.CharacterCodesCacheDx10.cache /game
+EvolutionUnpack H.CharacterCodesCacheDx11.cache /game
+EvolutionUnpack H.CharacterCodesCacheDx9.cache /game
+EvolutionUnpack H.Font.cache /game
+EvolutionUnpack H.Misc.cache /game
+EvolutionUnpack H.ShaderDx10.cache /game
+EvolutionUnpack H.ShaderDx11.cache /game
+EvolutionUnpack H.ShaderDx9.cache /game
+EvolutionUnpack H.ShaderPermutationDx10.cache /game
+EvolutionUnpack H.ShaderPermutationDx11.cache /game
+EvolutionUnpack H.ShaderPermutationDx9.cache /game
 echo Complete
 @pause
